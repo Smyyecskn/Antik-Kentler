807dbdd1321d405984789f7c12dc3c138e6ed1ee
smyye <smyyeoztrk43@gmail.com>
2024-09-17 16:51:10 +0300

    finished

--- src/components/AntikCard.jsx
+++ src/components/AntikCard.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import { Card } from "react-bootstrap";
 
 const AntikCard = ({ name, history, img }) => {
+  const [showImage, setShowImage] = useState(true);
+  // console.log(showImage);
+  const handleClick = () => {
+    setShowImage(!showImage);
+  };
+
   return (
-    <Card className="rounded-3 card ">
-      <Card.Img className="card-img" variant="top" src={img} />
+    <Card
+      className="rounded-3 card"
+      role="button"
+      // onClick={() => setShowImage(!showImage)}
+      onClick={handleClick}
+    >
+      {showImage ? (
+        <Card.Img className="card-img" variant="top" src={img} />
+      ) : (
+        <ul className="list-group list-group-flush">
+          {history.map((item, i) => (
+            <li key={i}>{item}</li>
+          ))}
+        </ul>
+      )}
+
       <Card.Footer>
         <Card.Title>{name}</Card.Title>
       </Card.Footer>
--- src/components/GeziCard.jsx
+++ src/components/GeziCard.jsx
@@ -4,22 +4,35 @@ import { data } from "../helper/data";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import AntikCard from "./AntikCard";
+import { useState } from "react";
 
 const GeziCard = () => {
   //   console.log("data", data);
+  const [search, setSearch] = useState("");
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  console.log(search);
   return (
     <>
       <Form.Control
+        onChange={handleSearch}
         placeholder="Search a Ancient City"
         className="w-50 m-auto"
+        type="search"
       />
-      <Container className=" my-4 p-3 card-container">
+      <Container className="rounded-3 my-4 p-3 card-container">
         <Row className="justify-content-center g-4">
-          {data.map((item, i) => (
-            <Col xl={3} lg={4} md={6} key={i}>
-              <AntikCard {...item} />
-            </Col>
-          ))}
+          {data // inputtakı arama işlevini filter ile yaptık.
+            .filter((item) =>
+              item.name.toLowerCase().includes(search.toLowerCase().trim())
+            )
+            .map((item, i) => (
+              <Col xl={3} lg={4} md={6} key={i}>
+                <AntikCard {...item} />
+              </Col>
+            ))}
         </Row>
       </Container>
     </>
